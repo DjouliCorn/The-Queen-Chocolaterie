e014f44b878820d83848da2880fd0fe89b05c9a0
DjouliCorn <tissier.julie@gmail.com>
2021-01-06 12:45:04 +0100

    barre de recherche

---
+++ web2reco/client/rechercher.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Rechercher un produit</title>
+</head>
+<body>
+
+<h1>Rechercher un produit</h1>
+<form method="get" action="resultat_rechercher.php">
+
+<input type="text" name="chocolat" />
+<input type="submit" value="Rechercher" />
+
+</form>
+    
+</body>
+</html>
---
+++ web2reco/inc/accesBDD.php
@@ -0,0 +1,7 @@
+<?php
+define('DB_HOST', '127.0.0.1');
+define('DB_USER', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'chocolaterie');
+define('DB_DSN', 'mysql:dbname=' . DB_NAME . ';host=' . DB_HOST . ';port=3306;charset=UTF8');
+?>
