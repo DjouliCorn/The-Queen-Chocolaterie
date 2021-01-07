deecef6b46e548763dd5b60f1900b0a89f327eaf
Nicolas Maurice <nicolas.maurice@edu.itescia.fr>
2021-01-07 22:08:02 +0100

    Last commit

--- base2reco.sql
+++ base2reco.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : localhost
--- Généré le : mer. 06 jan. 2021 à 17:16
+-- Généré le : jeu. 07 jan. 2021 à 22:04
 -- Version du serveur :  10.4.17-MariaDB
--- Version de PHP : 7.4.13
+-- Version de PHP : 8.0.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,6 +24,29 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Structure de la table `Categories`
+--
+
+CREATE TABLE `Categories` (
+  `idCategorie` int(11) NOT NULL,
+  `nomCategorie` char(50) DEFAULT NULL,
+  `descriptionCat` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `Categories`
+--
+
+INSERT INTO `Categories` (`idCategorie`, `nomCategorie`, `descriptionCat`) VALUES
+(1, 'Chocolat noir', 'Contient un minimum de 43% de cacao'),
+(2, 'Chocolat au lait', 'Contient un minimum de 25% de cacao sec dégraissé, 14% de lait en poudre et 25% de matières grasses'),
+(3, 'Chocolat blanc', 'Contient au moins 20% de beurre de cacao et 14% de produits lactiques secs, dont 3,5% de matière grasse lactique.'),
+(4, 'Chocolat blond', 'Destiné à enrober des fourrages pour confectionner des pâtisseries ou des bonbons au chocolat'),
+(5, 'Chocolat de couverture', 'Destiné à enrober des fourrages pour confectionner des bonbons chocolat');
+
+-- --------------------------------------------------------
+
+--
 -- Structure de la table `Produits`
 --
 
@@ -32,7 +55,7 @@ CREATE TABLE `Produits` (
   `nomProd` char(50) DEFAULT NULL,
   `descriptionProd` text DEFAULT NULL,
   `prodEquitable` tinyint(1) DEFAULT NULL,
-  `idCategorie` int(11) DEFAULT NULL,
+  `idCategorie` int(11) NOT NULL,
   `prix` float DEFAULT NULL,
   `stock` int(11) DEFAULT NULL,
   `promotion` tinyint(1) DEFAULT NULL
@@ -51,7 +74,6 @@ INSERT INTO `Produits` (`idProd`, `nomProd`, `descriptionProd`, `prodEquitable`,
 (6, 'Mendiant zeste d’orange', 'Confiserie à l’orange avec un zeste d’orange au sommet', 0, 3, 0.4, 250, 1),
 (7, 'Gianduja', 'Confiserie à base de chocolat et noisettes finement broyées', 1, 2, 2.5, 421, 0),
 (8, 'Ferrero Rocher', 'Confiserie à base de chocolat, de nutella et noisettes finement broyée', 0, 2, 6.8, 340, 0),
-(9, 'Oeufs en chocolat noir', 'Confiserie au chocolat noir', 0, 1, 3.99, 110, 0),
 (10, 'Papillote Révillon', 'Bonbons au chocolat pour les fêtes', 0, 3, 3.4, 645, 0),
 (11, 'Lindt Lindor', 'Coque chocolat au lait et garniture chocolat blanc crémeux', 0, 2, 7.89, 130, 1),
 (12, 'Fèves cacao', 'Fèves pour l’enrobage en provenance du Guatemala', 1, 5, 7.2, 145, 0),
@@ -63,12 +85,17 @@ INSERT INTO `Produits` (`idProd`, `nomProd`, `descriptionProd`, `prodEquitable`,
 (18, 'Orangettes', 'Confiserie à l’orange', 0, 1, 3, 30, 1),
 (19, 'Bonbon Dulcey', 'Bonbon au chocolat avec un coeur de mandarine et une base en feuilletine', 0, 4, 6.25, 654, 1),
 (20, 'Tablette de chocolat blond NESTLE', 'Tablette de chocolat blond aux noisettes et amandes entières ', 0, 4, 2.89, 110, 0),
-(21, 'Galak', 'Tablette avec un dauphin dessus', 0, 3, 1.23, 358, 1),
-(22, 'chcooooooooo', 'lfkkg', 0, 1, 1.23, 24, 1);
+(21, 'Galak', 'Tablette avec un dauphin dessus', 0, 3, 1.23, 358, 1);
 
 --
 -- Index pour les tables déchargées
 --
+
+--
+-- Index pour la table `Categories`
+--
+ALTER TABLE `Categories`
+  ADD PRIMARY KEY (`idCategorie`);
 
 --
 -- Index pour la table `Produits`
@@ -85,7 +112,7 @@ ALTER TABLE `Produits`
 -- AUTO_INCREMENT pour la table `Produits`
 --
 ALTER TABLE `Produits`
-  MODIFY `idProd` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+  MODIFY `idProd` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 
 --
 -- Contraintes pour les tables déchargées
